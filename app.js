73940e7e66cc46efc8118275b20b6166f18b60bd
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 09:58:16 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // write code here
+ <script>
+        let input = parseInt(prompt("ใส่ตัวเลข:"));
+
+        switch (input) {
+        case 1:
+            alert("One");
+            break;
+        case 0:
+            alert("Zero");
+            break;
+        case -1:
+            alert("Minus");
+            break;
+        default:
+            alert("Invalid number");
+        }
+
+  </script>
